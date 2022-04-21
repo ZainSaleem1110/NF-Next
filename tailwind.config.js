b7817106b0d7958d33aabf604e50d50732e92d5a
ZainSaleem1110 <szain0120@gmail.com>
2022-04-21 23:25:16 +0500

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+  future: {
+    removeDeprecatedGapUtilities: true,
   },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'accent-1': '#333',
+      },
+    },
+  },
+  variants: {},
   plugins: [],
 }
